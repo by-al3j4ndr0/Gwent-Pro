a3f2c711cedb644215ff13d234fb791b0fe91439
by-al3j4ndr0 <86642277+by-al3j4ndr0@users.noreply.github.com>
2024-04-17 12:09:38 -0400

    Created the turns system and configured the enviroment

--- scripts/CardBase.cs
+++ scripts/CardBase.cs
@@ -3,6 +3,7 @@ using System;
 
 public class CardBase : MarginContainer
 {
+    // Initializing variables
     private static TemplarsCards templarsCards = new TemplarsCards();
     private string card_name;
     private string picture_name;
@@ -14,16 +15,17 @@ public class CardBase : MarginContainer
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        // Set variables values
         picture_name = templarsCards.leader[0];
         name = templarsCards.leader[1];
         type = templarsCards.leader[2];
         points = templarsCards.leader[4];
         description = templarsCards.leader[5];
-        GetNode<Sprite>("Card").Texture = GD.Load<Texture>($"res://assets/cards/templars/{picture_name}");
-        GetNode<Label>("Bars/TopBar/MidGap/Name").Text = name;
+        GetNode<Sprite>("Card").Texture = GD.Load<Texture>($"res://assets/cards/templars/{picture_name}"); // Set card image
+        GetNode<Label>("Bars/TopBar/MidGap/Name").Text = name; // Set card name
     }
 
-// Called every frame. 'delta' is the elapsed time since the previous frame.
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
         
--- scripts/Main.cs
+++ scripts/Main.cs
@@ -3,6 +3,8 @@ using System;
 
 public class Main : Node2D
 {
+    public string current_player = "templars";
+    
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -13,5 +15,22 @@ public class Main : Node2D
     public override void _Process(float delta)
     {
     
+    }
+
+    private void _on_FinishTurn_pressed()
+    {
+        if(current_player == "templars"){
+            GetNode<Camera2D>("TemplarsCam").Current = false;
+            GetNode<Camera2D>("RomansCam").Current = true;
+            GetNode<Sprite>("BoardBG").FlipV = true;
+            GetNode<TextureButton>("FinishTurn").RectRotation = 180;
+            current_player = "romans";
+        } else if(current_player == "romans"){
+            GetNode<Camera2D>("TemplarsCam").Current = true;
+            GetNode<Camera2D>("RomansCam").Current = false;
+            GetNode<Sprite>("BoardBG").FlipV = false;
+            GetNode<TextureButton>("FinishTurn").RectRotation = 0;
+            current_player = "templars";
+        }
     }
 }
